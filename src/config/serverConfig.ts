eb94eeed0072e80bbc4d9ca7d3880b97ec2d85cf
Shivam Dhaka <Shivam.Dhaka@schulergroup.com>
2024-06-27 10:15:57 +0530

    changed default port to 8040

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const serverConfig = {
-	PORT: process.env.PORT || 5000,
+	PORT: process.env.PORT || 8040,
 	JWT_SECRET: process.env.JWT_SECRET,
 	MONGO_URL: process.env.MONGO_URL,
 	DB_NAME: process.env.DB_NAME,
